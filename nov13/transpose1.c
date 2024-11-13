b388837a075b7a38f10de5c3764748fe153105b6
User User <mohd.20050020@gmail.com>
2024-11-13 13:22:01 +0400

    nov13

---
+++ nov13/transpose1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void transpose(int n,int arr[][n]){
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            int temp=arr[i][j];
+            arr[i][j]=arr[j][i];
+            arr[j][i]=temp;
+        }
+    }
+    printf("Transpose matrix\n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main(){
+    int n;
+    printf("Enter n:");
+    scanf("%d",&n);
+    int arr[n][n];
+    printf("Enter elements:\n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    transpose(n,arr);
+}
